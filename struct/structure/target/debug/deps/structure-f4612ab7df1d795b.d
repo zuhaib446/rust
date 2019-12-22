f474de5bea686d7b27bc18a2a01936a4c2dd8ed5
zuhaib akhtar <zuhaib.rustam@gmail.com>
2019-12-22 09:42:15 +0500

    struct

---
+++ struct/structure/target/debug/deps/structure-f4612ab7df1d795b.d
@@ -0,0 +1,5 @@
+E:\ruest_batch3\rust\struct\structure\target\debug\deps\structure-f4612ab7df1d795b.exe: src\main.rs
+
+E:\ruest_batch3\rust\struct\structure\target\debug\deps\structure-f4612ab7df1d795b.d: src\main.rs
+
+src\main.rs:
---
+++ struct/structure/target/debug/structure.d
@@ -0,0 +1,1 @@
+E:\ruest_batch3\rust\struct\structure\target\debug\structure.exe: E:\ruest_batch3\rust\struct\structure\src\main.rs
