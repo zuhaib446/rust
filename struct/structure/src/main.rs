9121bd47ba01e79eca22dc76598dc5a6080080b7
zuhaib akhtar <zuhaib.rustam@gmail.com>
2019-12-22 10:02:35 +0500

    STD2

--- struct/structure/src/main.rs
+++ struct/structure/src/main.rs
@@ -1,3 +1,26 @@
+
+#[derive(Debug)]
+struct Students {
+    name : String,
+    age : i8
+}
+
 fn main() {
-    println!("Hello, world!");
+     let std1 = Students{
+       name : "std 1".to_string(),
+       age  : 21,
+       };
+    
+       let std2 = Students{
+        name : "std 2".to_string(),
+        age  : 22,
+        };
+     
+       std_struct_fn(std1 , std2);
+    
+}
+
+
+fn std_struct_fn (user1:Students , user2:Students) {
+  println!("{:#?} {:#?}"  , user1 , user2);
 }
